c56e815d2a92ab80cfd884762ff83994e130da44
blupie01 <jeffrey.chou.89@gmail.com>
2017-03-30 19:31:59 -0400

    changed routes

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,34 @@
-var orm = require('../config/orm.js');
+"use strict";
 
-var burger = {
-	all: function(cb) {
-		orm.all('burgers', function(res) {
-			cb(res);
-		});
-	},
-	// Create function
-	create: function(cols, vals, cb) {
-		orm.create('burgers', cols, vals, function(res) {
-			cb(res);
-		});
-	},
-	// Update function
-	update: function(objColVals, burgerID, cb) {
-		orm.update('burgers', objColVals, burgerID, function(res) {
-			cb(res);
-		});
-	}
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN,
+    created_at: DataTypes.NOW
+  }, {
+    // don't add the timestamp attributes (updatedAt, createdAt)
+    timestamps: true,
+
+    updatedAt: false,
+    // don't delete database entries but set the newly added attribute deletedAt
+    // to the current date (when deletion was done). paranoid will only work if
+    // timestamps are enabled
+      //paranoid: true,
+
+    // don't use camelcase for automatically added attributes but underscore style
+    // so updatedAt will be updated_at
+    underscored: true,
+  
+
+    // disable the modification of tablenames; By default, sequelize will automatically
+    // transform all passed model names (first parameter of define) into plural.
+    // if you don't want that, set the following
+    freezeTableName: true,
+
+    // define the table's name
+    tableName: 'burgers',
+
+  });
+
+  return Burger;
 };
-
-module.exports = burger;
